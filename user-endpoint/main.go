3f0185d2a79a132a53bb8d0a285ff5e3d19e8452
Marcel Stolin <mstolin@mailbox.org>
2022-04-21 10:11:49 +0200

    Initialized all clients before starting the web server

--- user-endpoint/main.go
+++ user-endpoint/main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/mstolin/present-roulette/user-endpoint/clients"
+	"github.com/mstolin/present-roulette/user-endpoint/handler"
 )
 
 func main() {
+	userClient, mailClient, wishlistClient := initClients()
+
 	address, exists := os.LookupEnv("ADDRESS")
 	if !exists {
 		address = ":8080"
 	}
+	httpHandler := handler.NewHandler(userClient, mailClient, wishlistClient)
+	http.ListenAndServe(address, httpHandler)
+}
 
-	//httpHandler =
+func initClients() (clients.UserClient, clients.MailClient, clients.WishlistClient) {
+	userServiceUrl := os.Getenv("USER_SERVICE")
+	userClient, err := clients.NewUserClient(userServiceUrl)
+	if err != nil {
+		log.Fatalf("Could not init UserClient: %v", err)
+	}
 
-	wishlistService := os.Getenv("WISHLIST_SERVICE")
-	mailService := os.Getenv("MAIL_SERVICE")
-	userService := os.Getenv("USER_SERVICE")
+	mailServiceUrl := os.Getenv("MAIL_SERVICE")
+	mailClient, err := clients.NewMailClient(mailServiceUrl)
+	if err != nil {
+		log.Fatalf("Could not init MailClient: %v", err)
+	}
 
-	http.ListenAndServe(address, nil)
+	wishlistServiceUrl := os.Getenv("WISHLIST_SERVICE")
+	wishlistClient, err := clients.NewWishlistClient(wishlistServiceUrl)
+	if err != nil {
+		log.Fatalf("Could not init WishlistClient: %v", err)
+	}
+
+	return userClient, mailClient, wishlistClient
 }
