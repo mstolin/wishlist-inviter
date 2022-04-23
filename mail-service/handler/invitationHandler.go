0a73c5847c9538f1a16aab98c3a5bac5357020df
Marcel Stolin <mstolin@mailbox.org>
2022-04-23 11:36:13 +0200

    Used dbclient and message factory first before sending an invitation

--- mail-service/handler/invitationHandler.go
+++ mail-service/handler/invitationHandler.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
-	"github.com/mstolin/present-roulette/mail-service/models"
 	"github.com/mstolin/present-roulette/utils/errors"
+	"github.com/mstolin/present-roulette/utils/models"
 )
 
 func invitationHandler(r chi.Router) {
@@ -15,21 +15,71 @@ func invitationHandler(r chi.Router) {
 
 // Send invitation endpoint
 func sendInvitation(w http.ResponseWriter, r *http.Request) {
-	invitation := models.Invitation{}
-
-	if error := render.Bind(r, &invitation); error != nil {
+	invitationReq := models.InvitationReq{}
+	if error := render.Bind(r, &invitationReq); error != nil {
 		render.Render(w, r, errors.ErrBadRequest)
 		return
 	}
 
-	gmailResp, error := gmailClientInstance.PostInvitation(invitation)
-	if error != nil {
-		render.Render(w, r, errors.ErrorRenderer(error))
+	// Get all items
+	items, err := getItemsForUser(invitationReq.UserId, invitationReq.Items)
+	if err != nil {
+		render.Render(w, r, errors.ErrorRenderer(err))
 		return
 	}
 
-	if error := render.Render(w, r, &gmailResp); error != nil {
-		render.Render(w, r, errors.ServerErrorRenderer(error))
+	// Generate invitation
+	invitation := generateInvitation(invitationReq.Subject, invitationReq.Recipient, items)
+
+	// Send Invitation
+	gmailResp, err := gmailClientInstance.SendInvitation(invitation)
+	if err != nil {
+		render.Render(w, r, errors.ErrorRenderer(err))
 		return
+	}
+
+	if err := render.Render(w, r, &gmailResp); err != nil {
+		render.Render(w, r, errors.ServerErrorRenderer(err))
+		return
+	}
+}
+
+// Returns an array of all requested user items
+func getItemsForUser(userId string, wantedIds []uint) ([]models.Item, error) {
+	itemLst, err := dbClientInstance.GetItemsForUser(userId)
+	if err != nil {
+		return []models.Item{}, err
+	}
+	return filterItems(itemLst.Items, wantedIds), nil
+}
+
+// Filters an array of items based on their IDs
+func filterItems(items []models.Item, wantedIds []uint) []models.Item {
+	filteredItems := []models.Item{}
+	for _, item := range items {
+		if contains(item.ID, wantedIds) {
+			filteredItems = append(filteredItems, item)
+		}
+	}
+	return filteredItems
+}
+
+// Check if the wanted number is in the given array.
+func contains(search uint, array []uint) bool {
+	for _, id := range array {
+		if search == id {
+			return true
+		}
+	}
+	return false
+}
+
+// Creates an instance if Invitation.
+func generateInvitation(subject string, recipient string, items []models.Item) models.Invitation {
+	message := msgFactoryInstance.GenerateInvitationMessage(items)
+	return models.Invitation{
+		Subject:   subject,
+		Recipient: recipient,
+		Message:   message,
 	}
 }
