275bcf3b21dc3dcfd7081bfcc8e5596e8745c36b
Marcel Stolin <mstolin@mailbox.org>
2022-04-23 11:35:41 +0200

    Created instances of new client

--- mail-service/handler/handler.go
+++ mail-service/handler/handler.go
@@ -6,14 +6,21 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
+	"github.com/mstolin/present-roulette/mail-service/database"
 	"github.com/mstolin/present-roulette/mail-service/gmail"
+	"github.com/mstolin/present-roulette/mail-service/messages"
 	"github.com/mstolin/present-roulette/utils/errors"
 )
 
 var gmailClientInstance gmail.GMailClient
+var dbClientInstance database.DatabaseClient
+var msgFactoryInstance messages.MessageFactory
 
-func NewHandler(gmailClient gmail.GMailClient) http.Handler {
+func NewHandler(gmailClient gmail.GMailClient, dbClient database.DatabaseClient, msgFactory messages.MessageFactory) http.Handler {
 	gmailClientInstance = gmailClient
+	dbClientInstance = dbClient
+	msgFactoryInstance = msgFactory
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
