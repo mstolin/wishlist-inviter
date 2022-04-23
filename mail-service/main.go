16674f81b41aeedb8192f89619a361670a24ec9d
Marcel Stolin <mstolin@mailbox.org>
2022-04-23 11:34:44 +0200

    Added dbclient and message factory to main.go

--- mail-service/main.go
+++ mail-service/main.go
@@ -5,21 +5,32 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/mstolin/present-roulette/mail-service/database"
 	"github.com/mstolin/present-roulette/mail-service/gmail"
 	"github.com/mstolin/present-roulette/mail-service/handler"
+	"github.com/mstolin/present-roulette/mail-service/messages"
 )
 
 func main() {
+	// GMail client
 	gmailService := os.Getenv("GMAIL_SERVICE")
-	gmailClient, error := gmail.NewGMailClient(gmailService)
-	if error != nil {
-		log.Fatalf("Could not create GMail client: %v", error)
+	gmailClient, err := gmail.NewGMailClient(gmailService)
+	if err != nil {
+		log.Fatalf("Could not create GMail client: %v", err)
 	}
+	// DB client
+	dbService := os.Getenv("DATABASE_SERVICE")
+	dbClient, err := database.NewDatabaseClient(dbService)
+	if err != nil {
+		log.Fatalf("Could not create Database Client: %v", err)
+	}
+	// Message factory
+	msgFactory := messages.MessageFactory{}
 
 	address, exists := os.LookupEnv("ADDRESS")
 	if !exists {
 		address = ":8080"
 	}
-	httpHandler := handler.NewHandler(gmailClient)
+	httpHandler := handler.NewHandler(gmailClient, dbClient, msgFactory)
 	http.ListenAndServe(address, httpHandler)
 }
