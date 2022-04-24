b42ac0a4f83db2a97a0995fb6a45d205095876ce
Marcel Stolin <mstolin@mailbox.org>
2022-04-24 11:12:23 +0200

    Added endpoints to call the user-service

--- user-endpoint/clients/userClient.go
+++ user-endpoint/clients/userClient.go
@@ -1,19 +1,19 @@
 package clients
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 
-	"github.com/mstolin/present-roulette/user-endpoint/models"
+	"github.com/mstolin/present-roulette/utils/clients"
+	"github.com/mstolin/present-roulette/utils/models"
 )
 
 type UserClient struct {
-	URL string
+	URL        string
+	httpFacade clients.HTTPFacade
 }
 
+// Constructs a new user client instance.
 func NewUserClient(url string) (UserClient, error) {
 	client := UserClient{}
 	if url == "" {
@@ -22,35 +22,80 @@ func NewUserClient(url string) (UserClient, error) {
 		client.URL = url
 	}
 
+	client.httpFacade = clients.NewHTTPFacade()
 	return client, nil
 }
 
-func (client UserClient) createUser() (*models.User, error) {
-	user := &models.User{}
+// Sends a request to the user server to create an empty user.
+func (client UserClient) CreateEmptyUser() (models.User, error) {
+	user := models.User{}
+	url := fmt.Sprintf("%s/users", client.URL)
 
-	url := client.URL + "/mail/send"
-	jsonStr := []byte("{}")
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	jsonStr, err := json.Marshal("{}")
 	if err != nil {
 		return user, err
 	}
-	req.Header.Set("Content-Type", "application/json")
-
-	httpClient := &http.Client{}
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return user, err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
+	res, err := client.httpFacade.DoPost(url, jsonStr)
 	if err != nil {
 		return user, err
 	}
 
-	if err := json.Unmarshal(body, user); err != nil {
+	if err := json.Unmarshal(res, &user); err != nil {
 		return user, err
 	}
-
 	return user, nil
+}
+
+// Sends a GET request to the user service to receive the user given the ID.
+func (client UserClient) GetUser(userId string) (models.User, error) {
+	user := models.User{}
+	url := fmt.Sprintf("%s/users/%s", client.URL, userId)
+
+	res, err := client.httpFacade.DoGet(url)
+	if err != nil {
+		return user, err
+	}
+
+	if err := json.Unmarshal(res, &user); err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+// Returns all items of a specific user.
+func (client UserClient) GetUserItems(userId string) (models.ItemList, error) {
+	itemLst := models.ItemList{}
+	url := fmt.Sprintf("%s/users/%s/items", client.URL, userId)
+
+	res, err := client.httpFacade.DoGet(url)
+	if err != nil {
+		return itemLst, err
+	}
+
+	if err := json.Unmarshal(res, &itemLst); err != nil {
+		return itemLst, err
+	}
+	return itemLst, nil
+}
+
+// Adds an item list to a specific user.
+func (client UserClient) AddUserItems(userId string, items []models.Item) (models.ItemList, error) {
+	itemLst := models.ItemList{}
+	url := fmt.Sprintf("%s/users/%s/items", client.URL, userId)
+
+	update := models.ItemList{Items: items}
+	jsonStr, err := json.Marshal(update)
+	if err != nil {
+		return itemLst, err
+	}
+
+	res, err := client.httpFacade.DoPut(url, jsonStr)
+	if err != nil {
+		return itemLst, err
+	}
+
+	if err := json.Unmarshal(res, &itemLst); err != nil {
+		return itemLst, err
+	}
+	return itemLst, nil
 }
--- user-endpoint/handler/userHandler.go
+++ user-endpoint/handler/userHandler.go
@@ -1,30 +1,101 @@
 package handler
 
 import (
+	"context"
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
+	"github.com/mstolin/present-roulette/utils/errors"
+	"github.com/mstolin/present-roulette/utils/models"
 )
 
-func userHandler(router chi.Router) {
-	router.Post("/", createUser)
+const USER_ID_KEY = "userId"
+
+func userHandler(r chi.Router) {
+	r.Post("/", createUser)
+	r.Route("/{userId}", func(r chi.Router) {
+		r.Use(userCtx)
+		r.Get("/", getUser)
+
+		r.Route("/items", func(r chi.Router) {
+			r.Get("/", getUserItems)
+			r.Put("/", addUserItems)
+		})
+	})
+}
+
+func userCtx(nxt http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userId := chi.URLParam(r, USER_ID_KEY)
+		if userId == "" {
+			render.Render(w, r, errors.ErrorRenderer(fmt.Errorf("user ID is required")))
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), USER_ID_KEY, userId)
+		nxt.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 	// Create User
-	resp, err := dbClientInstance.CreateUser()
+	resp, err := userClientInstance.CreateEmptyUser()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %q\n", err)
-		render.Render(w, r, ErrorRenderer(err))
+		render.Render(w, r, errors.ErrorRenderer(err))
 		return
 	}
 
-	if error := render.Render(w, r, resp); error != nil { // TODO better success response
-		fmt.Fprintf(os.Stderr, "Error: %q\n", error)
-		render.Render(w, r, ServerErrorRenderer(error))
+	if err := render.Render(w, r, &resp); err != nil { // TODO better success response
+		render.Render(w, r, errors.ServerErrorRenderer(err))
+		return
+	}
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(USER_ID_KEY).(string)
+	user, err := userClientInstance.GetUser(userId)
+	if err != nil {
+		render.Render(w, r, errors.ErrNotFound)
+		return
+	}
+	if err := render.Render(w, r, &user); err != nil {
+		render.Render(w, r, errors.ServerErrorRenderer(err))
+		return
+	}
+}
+
+func getUserItems(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(USER_ID_KEY).(string)
+	itemLst, err := userClientInstance.GetUserItems(userId)
+	if err != nil {
+		render.Render(w, r, errors.ErrNotFound)
+		return
+	}
+	if err := render.Render(w, r, &itemLst); err != nil {
+		render.Render(w, r, errors.ServerErrorRenderer(err))
+		return
+	}
+}
+
+func addUserItems(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(USER_ID_KEY).(string)
+
+	itemLstReq := models.ItemList{}
+	if err := render.Bind(r, &itemLstReq); err != nil {
+		render.Render(w, r, errors.ErrBadRequest)
+		return
+	}
+
+	res, err := userClientInstance.AddUserItems(userId, itemLstReq.Items)
+	if err != nil {
+		render.Render(w, r, errors.ErrorRenderer(err))
+		return
+	}
+
+	if err := render.Render(w, r, &res); err != nil {
+		render.Render(w, r, errors.ServerErrorRenderer(err))
 		return
 	}
 }
