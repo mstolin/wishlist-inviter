aad741e30a5d73b2045d03b1877d683a7970d78f
Marcel Stolin <mstolin@mailbox.org>
2022-05-10 14:57:45 +0200

    Added cascade delete to many2many relationship

--- utils/models/user.go
+++ utils/models/user.go
@@ -11,7 +11,7 @@ type User struct {
 	ID        uuid.UUID `gorm:"primary_key; unique; type:uuid; column:id; default:uuid_generate_v4()" json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	Items     []Item    `gorm:"many2many:user_items" json:"items"`
+	Items     []Item    `gorm:"many2many:user_items; constraint:OnDelete:CASCADE;" json:"items"`
 }
 
 func (*User) TableName() string {
