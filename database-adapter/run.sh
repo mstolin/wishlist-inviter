c6abf29ec656c6815cbd75e298e3dd02bac41735
Marcel Stolin <mstolin@mailbox.org>
2022-05-10 12:45:14 +0200

    OPtimized run script for database-adapter

--- database-adapter/run.sh
+++ database-adapter/run.sh
@@ -1,7 +1,6 @@
-export DB_HOST="localhost"
-export DB_PORT="5432"
-export DB_USER="admin"
-export DB_PASSWORD="admin123"
-export DB_NAME="present-roulette"
-
+DB_HOST="localhost" \
+DB_PORT="5432" \
+DB_USER="admin" \
+DB_PASSWORD="admin123" \
+DB_NAME="present-roulette" \
 go run main.go
