60d5cea2581e322b95b5712f2dd04077c5fe009d
Marcel Stolin <mstolin@mailbox.org>
2022-06-25 20:44:28 +0200

    Removed unused logs

--- user-service/handler/userHandler.go
+++ user-service/handler/userHandler.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
@@ -41,13 +40,11 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	// Create User
 	resp, err := dbClientInstance.CreateUser()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %q\n", err)
 		render.Render(w, r, httpErrors.ErrBadRequestRenderer(err))
 		return
 	}
 
-	if error := render.Render(w, r, &resp); error != nil { // TODO better success response
-		fmt.Fprintf(os.Stderr, "Error: %q\n", error)
+	if error := render.Render(w, r, &resp); error != nil {
 		render.Render(w, r, httpErrors.ErrServerErrorRenderer(error))
 		return
 	}
