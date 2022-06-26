c6b5602cebd4bd0bc56421824bb3822be7e50364
Marcel Stolin <mstolin@mailbox.org>
2022-06-26 17:00:40 +0200

    Added functionality to fetch userid from user-service

--- frontend/src/helper/UserHelper.js
+++ frontend/src/helper/UserHelper.js
@@ -1,15 +1,12 @@
 export default class UserHelper {
-
   static async fetchUserId() {
-    const host = 'http://localhost:8080';
-    let url = `${host}/users`
-    return fetch(url, {method: 'POST'})
-      .then(response => {
-        if(!response.ok) {
-          throw new Error(`HTTP status: ${response.status}`);
-        }
-        return response.json();
-      });
+    const host = "http://localhost:8080";
+    let url = `${host}/users`;
+    return fetch(url, { method: "POST" }).then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP status: ${response.status}`);
+      }
+      return response.json();
+    });
   }
-
 }
