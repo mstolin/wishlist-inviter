ac68aa097a15cebe118a6527b03c50887d95ff2a
Marcel Stolin <mstolin@mailbox.org>
2022-08-14 10:26:15 +0200

    Updated to read JWT tokens from file

--- user-service/main.go
+++ user-service/main.go
@@ -13,13 +13,17 @@ func main() {
 	databaseUrl := os.Getenv("DATABASE_ADAPTER")
 	dbClient, err := clients.NewDatabaseClient(databaseUrl)
 	if err != nil {
-		log.Fatalf("Could not create Database Client: %v", err)
+		log.Fatalf("Could not create Database Client: %v\n", err)
 	}
 
 	address, exists := os.LookupEnv("ADDRESS")
 	if !exists {
 		address = ":8080"
 	}
-	httpHandler := handler.NewHandler("SUPER_SECRET", dbClient)
+	signKey, exists := os.LookupEnv("JWT_SIGN_KEY")
+	if !exists {
+		log.Fatalf("No sign key given\n")
+	}
+	httpHandler := handler.NewHandler(signKey, dbClient)
 	http.ListenAndServe(address, httpHandler)
 }
