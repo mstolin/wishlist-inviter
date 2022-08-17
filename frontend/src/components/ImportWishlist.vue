76c60cbc6dba01b9afff3005c170ff7f6672f564
Marcel Stolin <mstolin@mailbox.org>
2022-08-17 18:53:09 +0200

    Removed unused frontend

--- frontend/src/components/ImportWishlist.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <form>
-        <input type="text" />
-        <input type="submit" value="Import" />
-    </form>
-</template>
--- frontend/src/components/Wishlist.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <h2>Your Wishlist</h2>
-        <ul>
-            <il>First Product</il>
-        </ul>
-    </div>
-</template>
